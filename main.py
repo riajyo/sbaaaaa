171195f7b3ea65622f0e0eb4d9ad6102e6434051
Garrick Kwan <sy10276@syss.edu.hk>
2024-08-29 22:44:54 +0800

    almost final update

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ con.commit()
 # Ref:
 # - https://www.sqlite.org/datatype3.html
 # - https://sqlite.org/foreignkeys.html
+
 cur.execute("DROP TABLE IF EXISTS Task")
 cur.execute("DROP TABLE IF EXISTS List")
 con.commit()
@@ -30,9 +31,9 @@ cur.execute(
         TID INTEGER PRIMARY KEY AUTOINCREMENT,
         Name TEXT NOT NULL,
         Category TEXT,
-        Current BLOB DEFAULT FALSE ,
+        Current INTEGER DEFAULT 0 ,
         List_id INTEGER,
-        Priority BLOB DEFAULT FALSE,
+        Priority INTEGER DEFAULT 0 ,
         Deadline TEXT NOT NULL,
         FOREIGN KEY (List_id) REFERENCES List(LID)
     )
@@ -61,7 +62,8 @@ def display_tasks(stdscr):
         stdscr.addstr(0, 0, "there is no task")
     else:
         for index, row in enumerate(rows):
-            stdscr.addstr(index, 0, f"TID: {row[0]:4} Name: {row[1]:10} Category: {row[2]:10} Priority: {row[3]:3} Deadline: {row[4]:10}")
+            stdscr.addstr(index, 0, f"TID: {row[0]:4} Name: {row[1]:10} Category: {row[2]:10} Priority: {row[5]:10} Deadline: {row[6]}")
+            stdscr.addstr(10, 10, "Remineder: True is displayed as 1 and False is displayed as 0")    
     stdscr.refresh()
     stdscr.getch()
 
@@ -95,14 +97,15 @@ def add_task(stdscr):
     stdscr.refresh()
     List_id = stdscr.getstr().decode('utf-8')
     stdscr.clear()
-    stdscr.addstr(0, 0, "Input the priority(True or False):")
+    stdscr.addstr(0, 0, "Input the priority(Input 1 for Yes, 0 for No):")
     stdscr.refresh()
     priority = stdscr.getstr().decode('utf-8')
     stdscr.clear()
     stdscr.addstr(0, 0, "Input the Deadline:")
     stdscr.refresh()
     deadline = stdscr.getstr().decode('utf-8')
-    cur.execute("INSERT INTO Task (Name, Category, List_id) VALUES (?, ?, ?, ?, ?)", (Name, Category, List_id, priority, deadline))
+    cur.execute("INSERT INTO Task (Name, Category, List_id, Priority, Deadline) VALUES (?, ?, ?, ?, ?)", (Name, Category, List_id, priority, deadline))
+    con.commit()
 
 # Add new list
 def add_list(stdscr):
@@ -115,7 +118,11 @@ def add_list(stdscr):
     stdscr.refresh()
     Duration = stdscr.getstr().decode('utf-8')
     stdscr.clear()
-    cur.execute("INSERT INTO List (Name , Duration) Values (? , ? )",(List_Name, Duration)) 
+    try:
+        cur.execute("INSERT INTO List (Name , Duration) Values (? , ? )",(List_Name, Duration))
+        con.commit()
+    except sqlite3.Error as e:
+        stdscr.addstr(1, 0, f"Database error: {e}")
 
 # delete task
 def del_task(stdscr):
@@ -124,7 +131,8 @@ def del_task(stdscr):
     stdscr.refresh()
     Name = stdscr.getstr().decode('utf-8')
     stdscr.clear()
-    cur.execute("DELETE FROM Task WHERE Name = (?)",(Name))
+    cur.execute("DELETE FROM Task WHERE Name = (?)",(Name,))
+    con.commit()
 
 # update task to Complete
 def up_task(stdscr):
@@ -133,12 +141,41 @@ def up_task(stdscr):
     stdscr.refresh()
     Name = stdscr.getstr().decode('utf-8')
     stdscr.clear()
-    cur.execute("UPDATE Task SET Current = True WHERE Name = (?)",(Name))
+    try:
+        cur.execute("UPDATE Task SET Current = True WHERE Name = ?", (Name,))
+        con.commit() 
+        stdscr.addstr(1, 0, "Task is updated.")
+    except sqlite3.Error as e:
+        stdscr.addstr(1, 0, f"Database error: {e}")
 
 # list the uncompleted task
 def uncompleted(stdscr):
-    cur.execute("SELECT * FROM Task WHERE Current = FALSE")
+    res = cur.execute("SELECT * FROM Task WHERE Current = 0 ")
+    rows = res.fetchall()
+    stdscr.clear()
+    if not rows:
+        stdscr.addstr(0, 0, "All Tasks are completed!!")
+    else:
+        for index, row in enumerate(rows):
+            stdscr.addstr(index, 0, f"TID: {row[0]:4} Name: {row[1]:10} Category: {row[2]:10} Priority: {row[5]:10} Deadline: {row[6]}")
+            stdscr.addstr(10, 10, "True is displayed as 1 and False is displayed as 0")        
+    stdscr.refresh()
+    stdscr.getch()
 
+# list the task user need to do now asap
+def DoItNow(stdscr):
+    res = cur.execute("SELECT * FROM Task WHERE Current = 0 AND Priority = 1")
+    rows = res.fetchall()
+    stdscr.clear()
+    if not rows:
+        stdscr.addstr(0, 0, "All Tasks are completed!!")
+    else:
+        for index, row in enumerate(rows):
+            stdscr.addstr(index, 0, f"TID: {row[0]:4} Name: {row[1]:10} Category: {row[2]:10} Priority: {row[5]:10} Deadline: {row[6]}")
+            stdscr.addstr(5, 10, "Do it now !!!!!!")
+            stdscr.addstr(10, 10, "True is displayed as 1 and False is displayed as 0")        
+    stdscr.refresh()
+    stdscr.getch()
 
 # Main loop
 def main(stdscr):
@@ -149,7 +186,8 @@ def main(stdscr):
         {"text": "Add Task",         "y": 7, "x": 0, "action": add_task},
         {"text": "Delete Task",      "y": 9, "x": 0, "action": del_task},
         {"text": "Update Task",      "y": 11, "x": 0, "action": up_task},
-        {"text": "Display Uncompleted Task", "y": 13, "x": 0, "action": uncompleted}
+        {"text": "Display Uncompleted Task", "y": 13, "x": 0, "action": uncompleted},
+        {"text": "What you have to do asap", "y": 15, "x": 0, "action": DoItNow}
     ]
 
     curses.start_color()
